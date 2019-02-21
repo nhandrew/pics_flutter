22d1b6e9c945d9c6d85553ba7bf95f874af7b26e
Andrew Julow <andrewjulow@Andrews-iMac.local>
2019-02-21 04:32:00 -0500

    initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class App extends StatefulWidget {
+@override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        appBar: AppBar(
+          title: Text('Lets see some images!'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                counter +=1;
+                print(counter);
+              });
+            },
+            child: Icon(Icons.add)),
+      ),
+    );
+  }
+}
